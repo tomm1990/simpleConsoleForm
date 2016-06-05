295b513658211a721981c945f9183801de7b4132
glshahar <galsh20@gmail.com>
2016-06-05 16:51:51 +0300

    Menu update

--- MethodsInSoftwareTask3/RunAll/main.cpp
+++ MethodsInSoftwareTask3/RunAll/main.cpp
@@ -1,35 +1,8 @@
-#include "../Combox/Combox.h"
-#include "../Form/Form.h"
-#include "../Control/Root.h"
-#include "../Textbox/TextBox.h"
-#include "../RadioList/RadioList.h"
-#include "../Label/Label.h"
-#include "../Checklist/CheckList.h"
 
+#include "../CheckList/CheckList.h"
 
-int main(void)
+int main()
 {
-	int width = 10;
-	int widthhhhhh = 4312;
-
-	//Combox example
-	vector<string> list;
-	list.push_back("apple");
-	list.push_back("banana");
-	list.push_back("orange");
-	Combox combox(width, list);
-	combox.Show();
-	combox.listen();
-
-	//TextBox example
-	TextBox textBox(width);
-
-	//RadioList example
-	RadioList radioList(width);
-
-	//Label example
-	Label label(width);
-
-	//Checklist example
-	CheckList checklist(width);
+	vector<string> options = { "[ ] Tommy", "[ ] Golden", "[ ] Lady", "[ ] Boy" };
+	CheckList(10, 20, options);
 }
