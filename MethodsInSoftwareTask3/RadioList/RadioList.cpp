119487803b167359c93996c52a79cf6d4214a5ba
almogShenkar <almogassu@gmail.com>
2016-07-08 13:21:46 +0300

    radiolist

--- MethodsInSoftwareTask3/RadioList/RadioList.cpp
+++ MethodsInSoftwareTask3/RadioList/RadioList.cpp
@@ -2,6 +2,30 @@
 
 RadioList::RadioList(int height,int width, vector<string> options) : Scrollable(width,options){
 	this->height = height;
+	auto e = [&](Control*c)
+	{
+		auto btn = dynamic_cast<Button*>(c);
+		auto word = btn->getValue().substr(3);
+		if (1)
+		{
+			btn->setValue("(*)" + word);
+		}
+		else
+		{
+			btn->setValue("( )" + word);
+		}
+		valsMap[word] = !valsMap[word];
+
+	};
+	for (auto i = 0; i<options.size(); ++i)
+	{
+		auto b = new Button(width);
+		b->setValue("( )" + options[i]);
+		b->addListener(e, b);
+		addControl(*b, 0, i);
+
+	}
+
 }
 
 void RadioList::update(size_t index)
