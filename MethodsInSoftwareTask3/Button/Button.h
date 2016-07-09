020f6fae4d70eb43dd869c3cfc6bd77b7a1aeb87
tomm1990 <tomg3210@gmail.com>
2016-07-09 18:40:38 +0300

    FINAL VERSION 2.2

--- MethodsInSoftwareTask3/Button/Button.h
+++ MethodsInSoftwareTask3/Button/Button.h
@@ -11,6 +11,7 @@ public:
 	void keyDown(const WORD code, const CHAR) override { if (code == VK_SPACE || code == VK_RETURN) action(c); }
 	void addListener(const std::function<void(Control*)> listener, Control* c)  { action = listener; this->c = c; }
 	void mousePressed(const int, const int, const bool) override { action(c); }
+
 private:
 	std::function<void(Control*)> action;
 	Control* c;
